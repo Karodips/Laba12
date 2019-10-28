ac04b2b8b43455214dcd4868c944c4cf81771279
Karodips <54810893+Karodips@users.noreply.github.com>
2019-10-28 13:24:40 +0300

    Update N1.cpp

--- N1.cpp
+++ N1.cpp
@@ -1,4 +1,6 @@
+
 #include <iostream>
+#include "Source.h"
 
 using namespace std;
 
@@ -7,10 +9,15 @@ int main() {
 	int a, b, i;
 	for (i = 1; i <= 5; i++) {
 		cin >> a;
-		b = a * a * a;
+		PowerA3(b, a);
 		cout << b << endl;
 	}
 	cout << endl;
 	system("pause");
 	return 0;
+}
+
+void PowerA3(int &b, int a)
+{
+	b = a * a * a;
 }
