2c196591260b637870c813a23bf37e6e18f96663
Karodips <54810893+Karodips@users.noreply.github.com>
2019-10-28 13:28:27 +0300

    Update N2.cpp

--- N2.cpp
+++ N2.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
+#include "Source.h"
 
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int a, x,i;
+	int a, x, i;
 	x = 0;
 	for (i = 0; i != 2; i++) {
 		cin >> a;
-		if (a > 0) a = 1;
-		else if (a < 0) a = -1;
-		else a = 0;
+		Sign(a);
 		x = x + a;
 	}
 	cout << x << endl;
 	system("pause");
 	return 0;
+}
+
+void Sign(int &a)
+{
+	if (a > 0) a = 1;
+	else if (a < 0) a = -1;
+	else a = 0;
 }
