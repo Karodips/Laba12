247c29669465491d22e8c518057d18143649d1cb
Karodips <54810893+Karodips@users.noreply.github.com>
2019-10-28 13:30:08 +0300

    Update N3.cpp

--- N3.cpp
+++ N3.cpp
@@ -1,4 +1,6 @@
+
 #include <iostream>
+#include "Source.h"
 
 using namespace std;
 
@@ -7,12 +9,17 @@ int main() {
 	float r1, r2, kolc;
 	cin >> r1 >> r2;
 	if (r1 > r2) {
-		r1 = r1 * r1 * 3.14;
-		r2 = r2 * r2 * 3.14;
-		kolc = r1 - r2;
+		Ring(r1, r2, kolc);
 		cout << kolc << endl;
 	}
 	else cout << "Nope" << endl;
 	system("pause");
 	return 0;
+}
+
+void Ring(float &r1, float &r2, float &kolc)
+{
+	r1 = r1 * r1 * 3.14;
+	r2 = r2 * r2 * 3.14;
+	kolc = r1 - r2;
 }
